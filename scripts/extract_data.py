cee23d4d0003386e57e7f48cf3f7ea493fa45611
rennertz <61323046+rennertz@users.noreply.github.com>
2021-03-24 23:05:03 -0300

    better declarative sintax

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -9,112 +9,99 @@ NOTICE
  - Novas features podem ser adicionadas em feat_functions.extract_features
 '''
 
+import sys
 import os
+from pathlib import Path
+import shutil
 import pandas as pd
-import numpy as np
-from feat_functions import extract_features
-import sys  # para imprimir status de importação
+from . import feat_functions
+import time
 
-import time  # permite marcar o tempo de execução
-start_time = time.time()
 
-# define barra de status a ser mostrada ao usuário
+def iterate_and_extract():
+    '''Função principal de extração. Entrará em cada pasta e irá extrair as características'''
+    
+    configure_normal_path()
+
+    # instancia o DataFrame com as primeiras colunas
+    df = pd.DataFrame(columns=['condicao', 'severidade', 'rotacao', 'fundamental'])
+
+    # abre a pasta de cada defeito
+    for condition in ['horizontal-misalignment', 'vertical-misalignment', 'imbalance', 'normal']:
+        num_severities = len(os.listdir(f"mafaulda/{condition}"))
+        
+        print('Carregando', condition)
+        instance_time = time.time()
+
+        # abre a subpasta de cada severidade do defeito
+        for i, severity in enumerate(os.listdir(f"mafaulda/{condition}")):
+            num_rotations = len(os.listdir(f"mafaulda/{condition}/{severity}"))
+            
+            severity_numeric = severity.replace('g', '').replace('mm', '')
+            severity_numeric = float(severity_numeric)
+
+            # abre cada arquivo, cujo nome indica a velocidade de rotação
+            for j, rotation in enumerate(os.listdir(f"mafaulda/{condition}/{severity}")):
+                
+                # exibe barra de status para o usuário (de acordo com o tipo de falha)
+                print_status_bar( (i + (j+1) / num_rotations) / num_severities)
+
+                # identifica o tipo de defeito, a severidade e a rotaçao
+                data = {'condicao': condition,
+                        'severidade': severity_numeric,
+                        'rotacao': float(rotation[:-4]) 
+                }
+
+                # extrai características (toma muito tempo!)
+                features = feat_functions.extract_features(f"mafaulda/{condition}/{severity}/{rotation}")
+                data.update(features)
+
+                # adiciona o novo dado ao dataframe
+                df = df.append(data, ignore_index=True)
+        
+
+        print('\n    Execução em {:.3f} segundos\n'.format(time.time()-instance_time))
+    
+    return df
+
+
 def print_status_bar(status):
+    '''define e exibe a barra de status ao usuário'''
+
     sys.stdout.write('\r')
     sys.stdout.write('    [{:20}] {:.3f}%'.format(
                      round(status*20)*'=', 100*status))
     sys.stdout.flush()
 
 
-# instancia o DataFrame com as primeiras colunas
-df = pd.DataFrame(columns=['defeito', 'severidade', 'rotacao', 'fundamental'])
+def configure_normal_path():
+    '''configura a pasta normal para sua correta digestão'''
 
-# abre a pasta de cada defeito
-for fault in ['horizontal-misalignment', 'vertical-misalignment', 'imbalance']:
-    print('\nCarregando', fault)
-    instance_time = time.time()
+    source_dir = "mafaulda/normal"
+    file_names = os.listdir(source_dir)
 
-    # abre a subpasta de cada severidade do defeito
-    for i, severity in enumerate(os.listdir("mafaulda/" + fault)):
+    target_dir = "mafaulda/normal/0"
+    Path(target_dir).mkdir(parents=True, exist_ok=True)
 
-        # abre cada arquivo, cujo nome indica a velocidade de rotação
-        for j, rotation in enumerate(os.listdir("mafaulda/" + fault + '/' + severity)):
-            
-            # identifica o tipo de defeito e assigna sua severidade 
-            # além disso, exibe barra de status para o usuário (de acordo com o tipo de falha)
-            if fault == 'horizontal-misalignment':
-                data = {'defeito': 'desalinhamento_horizontal',
-                        'severidade': float(severity[:-2]) }
-
-                print_status_bar((i+j/48)/4)
-
-            elif fault == 'vertical-misalignment':
-                data = {'defeito': 'desalinhamento_vertical',
-                        'severidade': float(severity[:-2]) }
-
-                print_status_bar((i+j/49)/6)
-
-            elif fault == 'imbalance':
-                data = {'defeito': 'desbalanceamento',
-                        'severidade': float(severity[:-1]) }
-
-                print_status_bar((i+j/48)/7)
-
-            
-            # recupera o nome do arquivo como rotaçao
-            data.update({'rotacao': float(rotation[:-4]) })
-
-            # extrai características (toma muito tempo!)
-            features = extract_features("mafaulda/{}/{}/{}".format(fault, severity, rotation))
-            data.update(features)
-
-            # adiciona o novo dado ao dataframe
-            df = df.append(data, ignore_index=True)
-    
-
-    print('\n    Execução em {:.3f} segundos'.format(
-          time.time()-instance_time))
+    for file_name in file_names:
+        shutil.move(os.path.join(source_dir, file_name), target_dir)
 
 
+if __name__ == '__main__':
 
-print('\nCarregando condição normal')
-instance_time = time.time()
+    # inicia a cronometragem
+    start_time = time.time()
 
-# abre a pasta de condição normal e abre cada arquivo
-for i, rotation in enumerate(os.listdir("mafaulda/normal")):
+    # extrai dados e exibe o resumo
+    df = iterate_and_extract()
+    df.info()
 
-    # identifica a condição normal e recupera o nome do arquivo como rotaçao
-    data = {'defeito': 'normal',
-            'severidade': 0,
-            'rotacao': float(rotation[:-4]) }
-    
+    # (sobre-)escreve um cvs com todos os dados
+    with open("data.csv", "w") as file:
+        df.to_csv(file, line_terminator='\n', index=False)
+    print('Dados salvos em: data.csv')
 
-    # extrai características (toma muito tempo!)
-    features = extract_features("mafaulda/normal/" + rotation)
-    data.update(features)
-
-    # exibe barra de status para o usuário
-    print_status_bar(i/48)
-
-    # adiciona o novo dado ao dataframe
-    df = df.append(data, ignore_index=True)
-    
-
-print('\n    Execução em {:.3f} segundos\n'.format(time.time()-instance_time))
-
-
-# Exibe o resumo dos dados
-df.info()
-
-
-# (sobre-)escreve um cvs com todos os dados
-f = open("../data.csv", "w")
-df.to_csv(f, line_terminator='\n', index=False)
-f.close()
-print('Dados salvos em: data.csv')
-
-
-# Exibe o tempo total de execução ao usuário
-total_duration = time.time()-start_time
-print('\nExecução total em {} minutos e {:.3f} segundos'.format(
-      int(total_duration//60), total_duration % 60))
+    # exibe o tempo de execução total ao usuário
+    total_duration = time.time()-start_time
+    print('\nExecução total em {} minutos e {:.3f} segundos'.format(
+          int(total_duration//60), total_duration % 60))
--- scripts/reduce_mafaulda.py
+++ scripts/reduce_mafaulda.py
@@ -8,13 +8,14 @@ Código responsável por compactar a base MaFaulDa ao
 import os
 import pandas as pd
 import numpy as np
-from feat_functions import extract_features
-
-import sys               # para imprimir status de importação
-from pathlib import Path # para permitir ao programa criar pastas
-
+import sys  # permite imprimir status de importação
+from pathlib import Path  # permite ao programa criar pastas
 import time  # permite marcar o tempo de execução
-start_time = time.time()
+
+# realiza a leitura dos sinais reduzindo a frequência da aquisição 'ratio' vezes, 
+# tomando apenas 1 a cada 'ratio' amostras temporais
+RATIO = 50
+
 
 # define barra de status a ser mostrada ao usuário
 def print_status_bar(status):
@@ -24,15 +25,10 @@ def print_status_bar(status):
     sys.stdout.flush()
 
 
-# realiza a leitura dos sinais reduzindo a frequência da aquisição 'ratio' vezes, 
-# tomando apenas 1 a cada 'ratio' amostras temporais
-RATIO = 50
-
 def read_reduced_csv(file_path, ratio = RATIO):
     # poupa para a leitura apenas as linhas múltiplas de 'ratio' e lista as demais para exclusão em 'skip'
     skip = [i for i in range(0, 250000) if i % ratio]
     # Nova frequência de aquisição. Note: 50 kHz é a frequência de aquisição original dos dados! 
-    sampling_freq = 50000/ratio
 
     signals = pd.read_csv(
         file_path, 
@@ -63,105 +59,69 @@ def generate_fft(signals, ratio = RATIO):
 
 # (sobre-)escreve um cvs com todos os dados
 def save_file(file_path, df):
-    f = open(file_path, "w")
-    df.to_csv(f, line_terminator='\n', float_format = "%.4f" , index=False)
-    f.close()
+    with open(file_path, "w") as f:
+        df.to_csv(f, line_terminator='\n', float_format = "%.4f" , index=False)
+    
     print('    Dados salvos em: ' + file_path)
 
 
+def iterate_and_reduce():
 
-# cria estrutura de pastas que receberá os novos arquivos
-for folder in ['horizontal-misalignment', 'vertical-misalignment', 'imbalance']:
-    Path("../mafaulda_reduced/"+folder).mkdir(parents=True, exist_ok=True)
+    # cria estrutura de pastas que receberá os novos arquivos
+    for folder in ['horizontal-misalignment', 'vertical-misalignment', 'imbalance', 'normal']:
+        Path("../mafaulda_reduced/"+folder).mkdir(parents=True, exist_ok=True)
 
 
-# abre a pasta de cada defeito
-for fault in ['horizontal-misalignment', 'vertical-misalignment', 'imbalance']:
-    print('\nCarregando', fault)
-    instance_time = time.time()
+    # abre a pasta de cada defeito
+    for condition in ['horizontal-misalignment', 'vertical-misalignment', 'imbalance', 'normal']:
+        num_severities = len(os.listdir(f"mafaulda/{condition}"))
 
-    # abre a subpasta de cada severidade do defeito
-    for i, severity in enumerate(os.listdir("mafaulda/" + fault)):
-        
-        # instancia os DataFrames
-        df_time = pd.DataFrame(columns=['rotacao'])
-        df_freq = pd.DataFrame(columns=['rotacao'])
+        print('\nCarregando', condition)
+        instance_time = time.time()
 
-        # abre cada arquivo, cujo nome indica a velocidade de rotação
-        for j, rotation in enumerate(os.listdir("mafaulda/" + fault + '/' + severity)):
-
-            # exibe barra de status para o usuário (de acordo com o tipo de falha)
-            if fault == 'horizontal-misalignment':
-                print_status_bar((i+j/48)/4)
-
-            elif fault == 'vertical-misalignment':
-                print_status_bar((i+j/49)/6)
-
-            elif fault == 'imbalance':
-                print_status_bar((i+j/48)/7)
+        # abre a subpasta de cada severidade do defeito
+        for i, severity in enumerate(os.listdir("mafaulda/" + condition)):
+            num_rotations = len(os.listdir(f"mafaulda/{condition}/{severity}"))
             
-            # aponta o arquivo e faz a leitura resumida
-            file_path = "mafaulda/{}/{}/{}".format(fault, severity, rotation)
-            signals = read_reduced_csv(file_path)
-            signals_fft = generate_fft(signals)
+            # instancia os DataFrames
+            df_time = pd.DataFrame(columns=['rotacao'])
+            df_freq = pd.DataFrame(columns=['rotacao'])
 
-            # recupera o nome do arquivo como rotaçao
-            signals['rotacao'] = rotation[:-4]
-            signals_fft['rotacao'] = rotation[:-4]
+            # abre cada arquivo, cujo nome indica a velocidade de rotação
+            for j, rotation in enumerate(os.listdir(f"mafaulda/{condition}/{severity}")):
 
-            # adiciona o novo dado ao dataframe
-            df_time = df_time.append(signals, ignore_index=True)
-            df_freq = df_freq.append(signals_fft, ignore_index=True)
+                # exibe barra de status para o usuário (de acordo com o tipo de falha)
+                print_status_bar( (i + (j+1) / num_rotations) / num_severities)
+                
+                # aponta o arquivo e faz a leitura resumida
+                signals = read_reduced_csv(f"mafaulda/{condition}/{severity}/{rotation}")
+                signals_fft = generate_fft(signals)
 
+                # recupera o nome do arquivo como rotaçao
+                signals['rotacao'] = rotation[:-4]
+                signals_fft['rotacao'] = rotation[:-4]
 
-        print(' ')
-        save_file('../mafaulda_reduced/{}/{}.csv'.format(fault, severity), df_time)
-        save_file('../mafaulda_reduced/{}/{}_fft.csv'.format(fault, severity), df_freq)
-        print('\n')
-
-
-    print('\n    Execução em {:.3f} segundos'.format(
-          time.time()-instance_time))
-
+                # adiciona o novo dado ao dataframe
+                df_time = df_time.append(signals, ignore_index=True)
+                df_freq = df_freq.append(signals_fft, ignore_index=True)
 
 
-print('\nCarregando condição normal')
-instance_time = time.time()
-
-# instancia os DataFrames
-df_time = pd.DataFrame(columns=['rotacao'])
-df_freq = pd.DataFrame(columns=['rotacao'])
-
-# abre a pasta de condição normal e abre cada arquivo
-for i, rotation in enumerate(os.listdir("mafaulda/normal")):
-
-    # exibe barra de status para o usuário
-    print_status_bar(i/48)
-
-    # aponta o arquivo e faz a leitura resumida
-    file_path = "mafaulda/normal/{}".format(rotation)
-    signals = read_reduced_csv(file_path)
-    signals_fft = generate_fft(signals)
-
-    # recupera o nome do arquivo como rotaçao
-    signals['rotacao'] = rotation[:-4]
-    signals_fft['rotacao'] = rotation[:-4]
-
-    # adiciona o novo dado ao dataframe
-    df_time = df_time.append(signals, ignore_index=True)
-    df_freq = df_freq.append(signals_fft, ignore_index=True)
-
-print(' ')
-save_file('../mafaulda_reduced/normal.csv'.format(fault, severity), df_time)
-save_file('../mafaulda_reduced/normal_fft.csv'.format(fault, severity), df_freq)
-print('\n')
-
-print('\n    Execução em {:.3f} segundos\n'.format(time.time()-instance_time))
+            print(' ')
+            save_file(f'../mafaulda_reduced/{condition}/{severity}.csv', df_time)
+            save_file(f'../mafaulda_reduced/{condition}/{severity}_fft.csv', df_freq)
+            print('\n')
 
 
+        print('\n    Execução em {:.3f} segundos'.format(
+            time.time()-instance_time))
 
 
-# Exibe o tempo total de execução ao usuário
-total_duration = time.time()-start_time
-print('\nExecução total em {} minutos e {:.3f} segundos'.format(
-      int(total_duration//60), total_duration % 60))
+if __name__ == "__main__":
+    start_time = time.time()
+
+    iterate_and_reduce()
+
+    # Exibe o tempo total de execução ao usuário
+    total_duration = time.time()-start_time
+    print('\nExecução total em {} minutos e {:.3f} segundos'.format(
+        int(total_duration//60), total_duration % 60))
